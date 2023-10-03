594223960f826d76b6ac7fc75174647d86a31759
othiel <othiel.leith@gmail.com>
2023-10-03 10:38:53 +0200

    quest6 DONE

--- userHandlers.js
+++ userHandlers.js
@@ -1,8 +1,24 @@
 
 const database = require("./database");
 const getUsers = (req, res) => {
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+  }
+  if (req.query.city) {
+    sql += " and city = ?";
+    sqlValues.push(req.query.city);
+  }
+//  else if (req.query.city) {
+//   sql += " where city = ?";
+//   sqlValues.push(req.query.max_duration);
+// }
+  
   database
-    .query("select * from users")
+    .query(sql, sqlValues)
     .then(([users]) => {
       res.json(users);
     })
@@ -11,6 +27,7 @@ const getUsers = (req, res) => {
       res.status(500).send("Error retrieving data from database");
     });
 };
+
 
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
