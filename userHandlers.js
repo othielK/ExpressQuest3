9efc9d02517b1e146f3e6071badd5db640c94c87
othiel <othiel.leith@gmail.com>
2023-10-03 10:44:18 +0200

    quests6

--- userHandlers.js
+++ userHandlers.js
@@ -4,18 +4,18 @@ const getUsers = (req, res) => {
   let sql = "select * from users";
   const sqlValues = [];
 
-  if (req.query.language) {
+  if (req.query.language!=null) {
     sql += " where language = ?";
     sqlValues.push(req.query.language);
   }
-  if (req.query.city) {
+  if (req.query.city!=null) {
     sql += " and city = ?";
     sqlValues.push(req.query.city);
   }
-//  else if (req.query.city) {
-//   sql += " where city = ?";
-//   sqlValues.push(req.query.max_duration);
-// }
+ else if (req.query.city!=null) {
+  sql += " where city = ?";
+  sqlValues.push(req.query.city);
+}
   
   database
     .query(sql, sqlValues)
